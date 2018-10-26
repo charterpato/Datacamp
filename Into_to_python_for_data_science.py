73d1b3c36031726175899fffe203a2715a1ad1c6
charterpato <44374219+charterpato@users.noreply.github.com>
2018-10-26 13:58:47 +0800

    Update Into_to_python_for_data_science.py

--- Into_to_python_for_data_science.py
+++ Into_to_python_for_data_science.py
@@ -1,3 +1,4 @@
+# ----- List Methods -----
 
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
@@ -8,7 +9,7 @@ print(areas.index(20.0))
 # Print out how often 9.50 appears in areas
 print(areas.count(9.50))
 
-* List Methods (2)
+# ----- List Methods (2) -----
 
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
