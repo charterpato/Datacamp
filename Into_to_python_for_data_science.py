0e02940bbf59a5ff205e8be87abec34325386cc0
charterpato <44374219+charterpato@users.noreply.github.com>
2018-10-26 14:10:06 +0800

    Update Into_to_python_for_data_science.py

--- Into_to_python_for_data_science.py
+++ Into_to_python_for_data_science.py
@@ -1,3 +1,112 @@
+
+
+
+# ----- Subsetting lists of lists -----
+
+x = [["a", "b", "c"],
+     ["d", "e", "f"],
+     ["g", "h", "i"]]
+x[2][0]
+x[2][:2]
+
+# ====== LIST MANIPULATION ====== 
+
+# ----- Replace list elements -----
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+# ----- Extend a list -----
+
+# Create the areas list and make some changes
+areas = ["hallway", 11.25, "kitchen", 18.0, "chill zone", 20.0,
+         "bedroom", 10.75, "bathroom", 10.50]
+
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+
+# ----- Delete list elements -----
+
+x = ["a", "b", "c", "d"]
+del(x[1])
+
+# ----- Inner workings of lists -----
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = areas[:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+
+# ====== FUNCTIONS ====== 
+
+# ----- Familiar functions -----
+
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+
+# ----- Help! -----
+
+help(max)
+?max
+
+# ----- Multiple arguments -----
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse = True)
+
+# Print out full_sorted
+print(full_sorted)
+
+# ====== METHODS ====== 
+
+# ----- String Methods -----
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count('o'))
+
 # ----- List Methods -----
 
 # Create list areas
