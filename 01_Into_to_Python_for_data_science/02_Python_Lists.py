b6d5d5b27c85c9c24f495afa24e1ac449017fa41
charterpato <44374219+charterpato@users.noreply.github.com>
2018-10-26 16:28:00 +0800

    Update 02_Python_Lists.py

--- 01_Into_to_Python_for_data_science/02_Python_Lists.py
+++ 01_Into_to_Python_for_data_science/02_Python_Lists.py
@@ -1,95 +1,4 @@
-# ====== 1. Hello Python! ======
-
-# ----- Any comments? -----
-
-# Division
-print(5 / 8)
-
-# Addition
-print(7 + 10)
-
-# ----- Python as a calculator -----
-
-# Addition, subtraction
-print(5 + 5)
-print(5 - 5)
-
-# Multiplication, division, modulo, and exponentiation
-print(3 * 5)
-print(10 / 2)
-print(18 % 7)
-print(4 ** 2)
-
-# How much is your $100 worth after 7 years?
-print(100 * 1.1 ** 7)
-
-# ====== 2. Variables & Types ======
-
-# ----- Variable Assignment -----
-
-# Create a variable savings
-savings = 100
-
-# Print out savings
-print(savings)
-
-# ----- Calculations with variables -----
-
-# Create a variable savings
-savings = 100
-
-# Create a variable factor
-factor = 1.10
-
-# Calculate result
-result = savings * factor ** 7
-
-# Print out result
-print(result)
-
-# ----- Other variable types -----
-
-# Create a variable desc
-desc = "compound interest"
-
-# Create a variable profitable
-profitable = True
-
-# ----- Operations with other types -----
-
-# Savings and factors
-savings = 100
-factor = 1.1
-desc = "compound interest"
-
-# Assign product of factor and savings to year1
-year1 = savings * factor
-
-# Print the type of year1
-print(type(year1))
-
-# Assign sum of desc and desc to doubledesc
-doubledesc = desc + desc
-
-# Print out doubledesc
-print(doubledesc)
-
-# ----- Type conversion -----
-
-# Definition of savings and result
-savings = 100
-result = 100 * 1.10 ** 7
-
-# Fix the printout
-print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
-
-# Definition of pi_string
-pi_string = "3.1415926"
-
-# Convert pi_string into float: pi_float
-pi_float = float(pi_string)
-
-# ====== 3. LISTS ====== 
+# ====== 1. LISTS ====== 
 
 # ----- Create a list -----
 
@@ -143,7 +52,7 @@ print(house)
 # Print out the type of house
 print(type(house))
 
-# ====== 4. SUBSETTING LISTS ====== 
+# ====== 2. SUBSETTING LISTS ====== 
 
 # ----- Subset and conquer -----
 
@@ -204,7 +113,7 @@ x = [["a", "b", "c"],
 x[2][0]
 x[2][:2]
 
-# ====== 5. LIST MANIPULATION ====== 
+# ====== 3. LIST MANIPULATION ====== 
 
 # ----- Replace list elements -----
 
@@ -246,87 +155,4 @@ areas_copy = areas[:]
 areas_copy[0] = 5.0
 
 # Print areas
-print(areas)
-
-# ====== 6. FUNCTIONS ====== 
-
-# ----- Familiar functions -----
-
-# Create variables var1 and var2
-var1 = [1, 2, 3, 4]
-var2 = True
-
-# Print out type of var1
-print(type(var1))
-
-# Print out length of var1
-print(len(var1))
-
-# Convert var2 to an integer: out2
-out2 = int(var2)
-
-# ----- Help! -----
-
-help(max)
-"?max"
-
-# ----- Multiple arguments -----
-
-# Create lists first and second
-first = [11.25, 18.0, 20.0]
-second = [10.75, 9.50]
-
-# Paste together first and second: full
-full = first + second
-
-# Sort full in descending order: full_sorted
-full_sorted = sorted(full, reverse = True)
-
-# Print out full_sorted
-print(full_sorted)
-
-# ====== 7. METHODS ====== 
-
-# ----- String Methods -----
-
-# string to experiment with: place
-place = "poolhouse"
-
-# Use upper() on place: place_up
-place_up = place.upper()
-
-# Print out place and place_up
-print(place)
-print(place_up)
-
-# Print out the number of o's in place
-print(place.count('o'))
-
-# ----- List Methods -----
-
-# Create list areas
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Print out the index of the element 20.0
-print(areas.index(20.0))
-
-# Print out how often 9.50 appears in areas
-print(areas.count(9.50))
-
-# ----- List Methods (2) -----
-
-# Create list areas
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Use append twice to add poolhouse and garage size
-areas.append(24.5)
-areas.append(15.45)
-
-# Print out areas
-print(areas)
-
-# Reverse the orders of the elements in areas
-areas.reverse()
-
-# Print out areas
 print(areas)
