a7c393ad75f29fb0d35ca68bb38e18e529c665c4
charterpato <patrickwang.xz@gmail.com>
2018-10-29 19:49:05 +0800

    Update

--- 03_Python_Data_Science_Toolbox_Part_1/01_Writing_your_own_functions.py
+++ 03_Python_Data_Science_Toolbox_Part_1/01_Writing_your_own_functions.py
@@ -1,2 +1,71 @@
+# ===== SCOPE AND USER-DEFINED FUNCTIONS =====
 
+# ----- The keyword global -----
+
+# Create a string: team
+team = "teen titans"
+
+# Define change_team()
+def change_team():
+    """Change the value of the global variable team."""
+
+    # Use team in global scope
+    global team
+
+    # Change the value of team in global: team
+    team = "justice league"
     
+# Print team
+print(team)
+
+# Call change_team()
+change_team()
+
+# Print team
+print(team)
+
+
+# ===== NESTED FUNCTIONS =====
+
+# ----- Nested Functions I -----
+
+# Define three_shouts
+def three_shouts(word1, word2, word3):
+    """Returns a tuple of strings
+    concatenated with '!!!'."""
+
+    # Define inner
+    def inner(word):
+        """Returns a string concatenated with '!!!'."""
+        return word + '!!!'
+
+    # Return a tuple of strings
+    return (inner(word1), inner(word2), inner(word3))
+
+# Call three_shouts() and print
+print(three_shouts('a', 'b', 'c'))
+
+# ----- Nested Functions II -----
+
+# Define echo
+def echo(n):
+    """Return the inner_echo function."""
+
+    # Define inner_echo
+    def inner_echo(word1):
+        """Concatenate n copies of word1."""
+        echo_word = word1 * n
+        return echo_word
+
+    # Return inner_echo
+    return inner_echo
+
+# Call echo: twice
+twice = echo(2)
+
+# Call echo: thrice
+thrice = echo(3)
+
+# Call twice() and thrice() then print
+print(twice('hello'), thrice('hello'))
+
