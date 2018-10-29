63888e725bb4ae5203fef2b3d1d75ef7ba4184f4
charterpato <patrickwang.xz@gmail.com>
2018-10-29 20:11:07 +0800

    Update

--- 03_Python_Data_Science_Toolbox_Part_1/01_Writing_your_own_functions.py
+++ 03_Python_Data_Science_Toolbox_Part_1/01_Writing_your_own_functions.py
@@ -1,71 +1,166 @@
-# ===== SCOPE AND USER-DEFINED FUNCTIONS =====
+# ===== 1. USER_DEFINED FUNCTIONS =====
 
-# ----- The keyword global -----
+# ----- Write a simple function -----
 
-# Create a string: team
-team = "teen titans"
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
 
-# Define change_team()
-def change_team():
-    """Change the value of the global variable team."""
+    # Print shout_word
+    print(shout_word)
 
-    # Use team in global scope
-    global team
+# Call shout
+shout()
 
-    # Change the value of team in global: team
-    team = "justice league"
+# ----- Single-parameter functions -----
+
+# Define shout with the parameter, word
+def shout(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout('congratulations')
+
+# ----- Functions that return single values -----
+
+# Define shout with the parameter, word
+def shout(word):
+    """Return a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Replace print with return
+    return(shout_word)
+
+# Pass 'congratulations' to shout: yell
+yell = shout('congratulations')
+
+# Print yell
+print(yell)
+
+
+# ===== 2. MULTIPLE PARAMETERS AND RETURN VALUES =====
+
+# ----- Functions with multiple parameters -----
+
+# Define shout with parameters word1 and word2
+def shout(word1, word2):
+    """Concatenate strings with three exclamation marks"""
+    # Concatenate word1 with '!!!': shout1
+    shout1 = word1 + '!!!'
     
-# Print team
-print(team)
+    # Concatenate word2 with '!!!': shout2
+    shout2 = word2 + '!!!'
+    
+    # Concatenate shout1 with shout2: new_shout
+    new_shout = shout1 + shout2
 
-# Call change_team()
-change_team()
+    # Return new_shout
+    return new_shout
 
-# Print team
-print(team)
+# Pass 'congratulations' and 'you' to shout(): yell
+yell = shout('congratulations', 'you')
+
+# Print yell
+print(yell)
+
+# ----- A brief introduction to tuples -----
+
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+
+# Construct even_nums
+even_nums = (2, num2, num3)
+
+# ----- Functions that return multiple values -----
+
+# Define shout_all with parameters word1 and word2
+def shout_all(word1, word2):
+    
+    # Concatenate word1 with '!!!': shout1
+    shout1 = word1 + '!!!'
+    
+    # Concatenate word2 with '!!!': shout2
+    shout2 = word2 + '!!!'
+    
+    # Construct a tuple with shout1 and shout2: shout_words
+    shout_words = (shout1, shout2)
+
+    # Return shout_words
+    return shout_words
+
+# Pass 'congratulations' and 'you' to shout_all(): yell1, yell2
+yell1, yell2 = shout_all('congratulations', 'you')
+
+# Print yell1 and yell2
+print(yell1)
+print(yell2)
 
 
-# ===== NESTED FUNCTIONS =====
+# ===== 3. BRINGING IT ALL TOGETHER =====
 
-# ----- Nested Functions I -----
+# ----- Bringing it all together (1) -----
 
-# Define three_shouts
-def three_shouts(word1, word2, word3):
-    """Returns a tuple of strings
-    concatenated with '!!!'."""
+# Import pandas
+import pandas as pd
 
-    # Define inner
-    def inner(word):
-        """Returns a string concatenated with '!!!'."""
-        return word + '!!!'
+# Import Twitter data as DataFrame: df
+df = pd.read_csv('tweets.csv')
 
-    # Return a tuple of strings
-    return (inner(word1), inner(word2), inner(word3))
+# Initialize an empty dictionary: langs_count
+langs_count = {}
 
-# Call three_shouts() and print
-print(three_shouts('a', 'b', 'c'))
+# Extract column from DataFrame: col
+col = df['lang']
 
-# ----- Nested Functions II -----
+# Iterate over lang column in DataFrame
+for entry in col:
 
-# Define echo
-def echo(n):
-    """Return the inner_echo function."""
+    # If the language is in langs_count, add 1
+    if entry in langs_count.keys():
+        langs_count[entry] = langs_count[entry] + 1
+    # Else add the language to langs_count, set the value to 1
+    else:
+        langs_count[entry] = 1
 
-    # Define inner_echo
-    def inner_echo(word1):
-        """Concatenate n copies of word1."""
-        echo_word = word1 * n
-        return echo_word
+# Print the populated dictionary
+print(langs_count)
 
-    # Return inner_echo
-    return inner_echo
+# ----- Bringing it all together (2) -----
 
-# Call echo: twice
-twice = echo(2)
+# Define count_entries()
+def count_entries(df, col_name):
+    """Return a dictionary with counts of 
+    occurrences as value for each key."""
 
-# Call echo: thrice
-thrice = echo(3)
+    # Initialize an empty dictionary: langs_count
+    langs_count = {}
+    
+    # Extract column from DataFrame: col
+    col = df[col_name]
+    
+    # Iterate over lang column in DataFrame
+    for entry in col:
 
-# Call twice() and thrice() then print
-print(twice('hello'), thrice('hello'))
+        # If the language is in langs_count, add 1
+        if entry in langs_count.keys():
+            langs_count[entry] = langs_count[entry] + 1
+        # Else add the language to langs_count, set the value to 1
+        else:
+            langs_count[entry] = 1
 
+    # Return the langs_count dictionary
+    return langs_count
+
+# Call count_entries(): result
+result = count_entries(tweets_df, 'lang')
+
+# Print the result
+print(result)
