4abc5898bd1062e15d503968fe3c2fe29c827232
charterpato <patrickwang.xz@gmail.com>
2018-11-17 17:10:03 +0800

    Update

--- 05_Importing_Data_in_Python_Part_1/02_Importing_data_from_other_file_types.py
+++ 05_Importing_Data_in_Python_Part_1/02_Importing_data_from_other_file_types.py
@@ -57,3 +57,120 @@ df2 = xl.parse(1, parse_cols=[0], skiprows=[0], names=['Country'])
 # Print the head of the DataFrame df2
 print(df2.head())
 
+# ===== 2. IMPORTING SAS/STATA FILES USING PANDAS =====
+
+# ----- Importing SAS files -----
+
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+# ----- Using read_stata to import Stata files -----
+
+df = pd.read_stata('disarea.dta')
+
+# ----- Importing Stata files -----
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+
+# ===== 3. IMPORTING HDF5 FILES =====
+
+# ----- Using h5py to import HDF5 files -----
+
+# Import packages
+import numpy as np
+import h5py
+
+# Assign filename: file
+file = 'LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Print the datatype of the loaded file
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+# ----- Extracting data from your HDF5 file -----
+
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
+
+# ===== 4. IMPORTING MATLAB FILES =====
+
+# ----- Loading .mat files -----
+
+# Import package
+import scipy.io
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+
+# Print the datatype type of mat
+print(type(mat))
+
+# ----- The structure of .mat in Python -----
+
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(np.shape(mat['CYratioCyt']))
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
+
