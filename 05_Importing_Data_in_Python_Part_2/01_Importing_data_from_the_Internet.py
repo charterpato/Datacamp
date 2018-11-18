a703e84b196cad9e9fb3ae6dc042d81988d94f5b
charterpato <patrickwang.xz@gmail.com>
2018-11-18 14:15:02 +0800

    Update

--- 05_Importing_Data_in_Python_Part_2/01_Importing_data_from_the_Internet.py
+++ 05_Importing_Data_in_Python_Part_2/01_Importing_data_from_the_Internet.py
@@ -1,4 +1,4 @@
-# ===== IMPORTING FLAT FILES FROM THE WEB =====
+# ===== 1. IMPORTING FLAT FILES FROM THE WEB =====
 
 # ----- Importing flat files from the web: your turn! -----
 
@@ -60,3 +60,151 @@ print(xl.keys())
 
 # Print the head of the first sheet (using its name, NOT its index)
 print(xl['1700'].head())
+
+
+# ===== 2. HTTP REQUESTS TO IMPORT FILES FROM THE WEB =====
+
+# ----- Performing HTTP requests in Python using urllib -----
+
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "http://www.datacamp.com/teach/documentation"
+
+# This packages the request: request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Print the datatype of response
+print(type(response))
+
+# Be polite and close the response!
+response.close()
+
+# ----- Printing HTTP request results in Python using urllib -----
+
+# Import packages
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "http://www.datacamp.com/teach/documentation"
+
+# This packages the request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+
+# Extract the response: html
+html = response.read()
+
+# Print the html
+print(html)
+
+# Be polite and close the response!
+response.close()
+
+# ----- Performing HTTP requests in Python using requests -----
+
+# Import package
+import requests
+
+# Specify the url: url
+url = 'http://www.datacamp.com/teach/documentation'
+
+# Packages the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response: text
+text = r.text
+
+# Print the html
+print(text)
+
+
+# ===== 3. SCRAPING THE WEB IN PYTHON =====
+
+# ----- Parsing HTML with BeautifulSoup -----
+
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Prettify the BeautifulSoup object: pretty_soup
+pretty_soup = soup.prettify()
+
+# Print the response
+print(pretty_soup)
+
+# ----- Turning a webpage into data using BeautifulSoup: getting the text -----
+
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extract the response as html: html_doc
+html_doc = r.text
+
+# Create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Get the title of Guido's webpage: guido_title
+guido_title = soup.title
+
+# Print the title of Guido's webpage to the shell
+print(guido_title)
+
+# Get Guido's text: guido_text
+guido_text = soup.get_text()
+
+# Print Guido's text to the shell
+print(guido_text)
+
+# ----- Turning a webpage into data using BeautifulSoup: getting the hyperlinks -----
+
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url
+url = 'https://www.python.org/~guido/'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+
+# create a BeautifulSoup object from the HTML: soup
+soup = BeautifulSoup(html_doc)
+
+# Print the title of Guido's webpage
+print(soup.title)
+
+# Find all 'a' tags (which define hyperlinks): a_tags
+a_tags = soup.find_all('a')
+
+# Print the URLs to the shell
+for link in a_tags:
+    print(link.get('href'))
